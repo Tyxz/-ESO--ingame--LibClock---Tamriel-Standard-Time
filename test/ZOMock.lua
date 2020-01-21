15b4608af2b5ba18f560a0721650bd8f389136a2
Arne Rantzen <Tyxz@users.noreply.github.com>
2020-01-21 22:58:46 +0000

    Removed sockets requirement for test

--- test/ZOMock.lua
+++ test/ZOMock.lua
@@ -3,17 +3,6 @@
 
 local updateListener = {}
 Dump = require("pl.pretty").dump
-Sleep = require("socket").sleep
-
-local Update = function(n)
-  for i = 1, n do
-    Sleep(1)
-    for _, v in pairs(updateListener) do
-      v()
-    end
-  end
-end
-
 
 GetTimeStamp = function() return os.time(os.date("!*t")) end
 
@@ -21,7 +10,7 @@ EVENT_MANAGER = {}
 
 function EVENT_MANAGER:RegisterForUpdate(eventHandle, updateDelay, OnUpdate)
   updateListener[eventHandle] = OnUpdate
-  Update(1)
+  OnUpdate()
 end
 
 function EVENT_MANAGER:UnregisterForUpdate(eventHandle) 
